c71ddeb3f69aa9e653c54fc58a60020302146c4e
wachtelhund <hampus.nilsson222@hotmail.se>
2023-11-14 20:11:51 +0100

    add test

--- test/Game.test.ts
+++ test/Game.test.ts
@@ -8,3 +8,10 @@ test("should throw error if number of players is less than 2", () => {
 test("should throw error if number of dice per player is less than 1", () => {
     expect(() => new Game(2, 0)).toThrow();
 });
+
+test("getPlayerIndex should return the index of the player with the current turn", () => {
+    const expected = 1;
+    const game = new Game(2, 1);
+    const actual = game.getPlayerIndex();
+    expect(actual).toBe(expected);
+});
