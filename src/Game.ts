2a36fb67c5652d88863fdf90a59af185064b082a
wachtelhund <hampus.nilsson222@hotmail.se>
2023-11-14 20:08:11 +0100

    implement

--- src/Game.ts
+++ src/Game.ts
@@ -1,3 +1,11 @@
 export class Game {
-    constructor(amountOfPlayers: number, dicePerPlayer: number) {}
+    constructor(amountOfPlayers: number, dicePerPlayer: number) {
+        if (amountOfPlayers < 2) {
+            throw new RangeError("Amount of players must be greater than 1");
+        }
+        if (dicePerPlayer < 1) {
+            throw new RangeError("Amount of dice per player must be greater than 0");
+        }
+    }
+
 }
--- test/Game.test.ts
+++ test/Game.test.ts
@@ -4,3 +4,7 @@ import { Game } from "../src/Game";
 test("should throw error if number of players is less than 2", () => {
     expect(() => new Game(1, 1)).toThrow();
 });
+
+test("should throw error if number of dice per player is less than 1", () => {
+    expect(() => new Game(2, 0)).toThrow();
+});
