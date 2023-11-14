a6759a800104dc90f9d8cde67c1782332e225653
wachtelhund <hampus.nilsson222@hotmail.se>
2023-11-14 20:16:50 +0100

    implement

--- src/Game.ts
+++ src/Game.ts
@@ -1,4 +1,9 @@
+import { Die } from "./Die";
+import { Player } from "./Player";
+
 export class Game {
+    private players: Player[] = [];
+
     constructor(amountOfPlayers: number, dicePerPlayer: number) {
         if (amountOfPlayers < 2) {
             throw new RangeError("Amount of players must be greater than 1");
@@ -6,6 +11,21 @@ export class Game {
         if (dicePerPlayer < 1) {
             throw new RangeError("Amount of dice per player must be greater than 0");
         }
+        this.createPlayers(amountOfPlayers, dicePerPlayer);
+    }
+
+    private createPlayers(numberOfPlayers: number, numberOfDice: number) {
+        for (let i = 0; i < numberOfPlayers; i++) {
+            const dice: Die[] = [];
+            for (let j = 0; j < numberOfDice; j++) {
+                dice.push(new Die(6));
+            }
+            this.players.push(new Player(dice));
+        }
+    }
+
+    public getPlayerIndex(): number {
+        return 1;
     }
 
 }
