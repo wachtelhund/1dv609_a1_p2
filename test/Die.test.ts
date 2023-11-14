86312ddf51ddbe170e95d455cea50738bd944f21
wachtelhund <hampus.nilsson222@hotmail.se>
2023-11-14 19:26:19 +0100

    add test

--- test/Die.test.ts
+++ test/Die.test.ts
@@ -7,3 +7,10 @@ test("getSides should return number of sides", () => {
     const actual = new Die(6).getSides();
     assert.equal(actual, expected);
 });
+
+test("value should be 1 if no rolls have occured", () => {
+    const expected = 1;
+    const die = new Die(6);
+    const actual = die.getValue();
+    assert.equal(actual, expected);
+});
