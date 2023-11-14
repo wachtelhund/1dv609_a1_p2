03f4cd5d89fc047b6731fefb5dbdf36f0446248a
wachtelhund <hampus.nilsson222@hotmail.se>
2023-11-14 19:27:13 +0100

    implement

--- src/Die.ts
+++ src/Die.ts
@@ -1,8 +1,14 @@
 export class Die {
+    private value: number = 1;
+
     constructor(private sides: number) {
     }
 
     getSides(): number {
         return this.sides;
+    }
+
+    getValue() {
+        return this.value;
     }
 }
