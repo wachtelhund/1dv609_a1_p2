e9bfd2bdb55213653858dc1c1837fed997c3b87b
wachtelhund <hampus.nilsson222@hotmail.se>
2023-11-14 19:55:20 +0100

    add test

--- test/Player.test.ts
+++ test/Player.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "bun:test";
 import { Player } from "../src/Player";
 import assert from "assert";
+import { Die } from "../src/Die";
 
 test("amount of dies should not be less than 1", () => {
     expect(() => new Player(0)).toThrow();
@@ -12,3 +13,22 @@ test("sum of dies should be equal to number of dies if no rolls have occured", (
     const actual = player.getSum();
     assert.equal(actual, expected);
 });
+
+test("getSum should return the sum of all dies", () => {
+    const expected = 6;
+    const player = new Player(3);
+    player.roll();
+    player.getSum();
+    const actual = player.getSum();
+    assert.equal(actual, expected);
+});
+
+class DieStub extends Die {
+    constructor() {
+        super(6);
+    }
+
+    roll() {
+        this.value = 2;
+    }
+}
