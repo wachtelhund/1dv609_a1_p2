3c5d3c929d917539a126c0f38a11fe03456775c2
wachtelhund <hampus.nilsson222@hotmail.se>
2023-11-14 19:49:29 +0100

    add test

--- test/Player.test.ts
+++ test/Player.test.ts
@@ -5,3 +5,10 @@ import assert from "assert";
 test("amount of dies should not be less than 1", () => {
     expect(() => new Player(0)).toThrow();
 });
+
+test("sum of dies should be 0 if no rolls have occured", () => {
+    const expected = 0;
+    const player = new Player(2);
+    const actual = player.getSum();
+    assert.equal(actual, expected);
+});
